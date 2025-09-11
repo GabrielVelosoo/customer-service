c6ffdc0e2d4dd2291ac5651a3136b1b8ce987bb5
Gabriel Veloso <gaabrielvelooso@gmail.com>
2025-09-10 21:03:36 -0300

    Initial commit: setup project structure

---
+++ src/main/java/io/github/gabrielvelosoo/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,21 @@
+package io.github.gabrielvelosoo.customerservice;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(CustomerServiceApplication.class);
+        builder.bannerMode(Banner.Mode.OFF);
+        builder.run(args);
+        ConfigurableApplicationContext context = builder.context();
+        ConfigurableEnvironment environment = context.getEnvironment();
+        String applicationName = environment.getProperty("spring.application.name");
+        System.out.println("Microservice name: " + applicationName);
+    }
+}
